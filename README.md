c44779b6ee333bb61c027916bc271469010ff5b5
Frederik Bode <thorbensen@gmail.com>
2025-10-30 23:07:15 +0100

    feat(main): getting back to this project

--- README.md
+++ README.md
@@ -8,3 +8,8 @@ header-only library for simulating asset movements
 tickstream/include/tickstream/stream_gen.hpp    # public
 tickstream/include/tickstream/detail/models.hpp # private
 ```
+
+
+### Concepts
+
+- [Ring Buffer](https://en.wikipedia.org/wiki/Circular_buffer)
--- src/main.cpp
+++ src/main.cpp
@@ -7,14 +7,32 @@
 #include <tickstream/params.hpp>
 #include <tickstream/stream_gen.hpp>
 
-int main() {
-    // create demo tick
-    auto t = tickstream::Tick();
-    t.symbol = "bitcoin";
-    t.price = 119200.0;
-    t.seq = 0;
-    t.ts = std::chrono::steady_clock::now();
-    t.regime = 0;
+void tick_btc()
+{
+    auto ts1 = tickstream::Tick();
+    ts1.symbol = "bitcoin";
+    ts1.price = 119200.0;
+    ts1.seq = 0;
+    ts1.ts = std::chrono::steady_clock::now();
+    ts1.regime = 0;
 
-    std::cout << "Latest tick: " << t.price << std::endl;
+    std::cout << "Latest tick: " << ts1.price << std::endl;
+}
+
+void tick_eth()
+{
+    auto ts2 = tickstream::Tick();
+    ts2.symbol = "ethereum";
+    ts2.price = 4099.71;
+    ts2.seq = 0;
+    ts2.ts = std::chrono::steady_clock::now();
+    ts2.regime = 0;
+
+    std::cout << "Latest tick: " << ts2.price << std::endl;
+}
+
+int main()
+{
+    tick_btc();
+    tick_eth();
 }
