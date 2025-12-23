48e55eccbf9d39a1ebba703c2898d80e324a3e8b
bodeby <thorbensen@gmail.com>
2025-12-23 16:53:43 +0100

    feat(test): added small sample test for CI

--- tests/tickstream/test_tick.cpp
+++ tests/tickstream/test_tick.cpp
@@ -4,3 +4,16 @@
 
 // internal includes
 #include <tickstream/tick.hpp>
+
+namespace ts = tickstream; // local alias
+
+TEST_CASE("Tick Creation Test", "[tick]")
+{
+    auto tick_1 = ts::tick_btc();
+    auto tick_2 = ts::tick_btc();
+
+    REQUIRE(tick_1.symbol == "BTC-USD");
+    REQUIRE(tick_2.symbol == "BTC-USD");
+    REQUIRE(tick_1.price > 0);
+    REQUIRE(tick_2.price > 0);
+}
